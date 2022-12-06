3f881ab36f51344cf36f1af25d456eed9bd01fad
Cadyn Reger <116835253+creger01@users.noreply.github.com>
2022-12-06 13:19:31 -0500

    Add files via upload

---
+++ Lab 1(real).py
@@ -0,0 +1,54 @@
+# the authors' name are: Sarah and Cadyn
+print("Hello, world")
+
+print("Welcome to CPSC 207")
+
+print("Hello\nWorld")
+
+print("How\nare\nyou?")
+
+print("Today", "is", "Tuesday")
+
+print("Today" + "is" + "Tuesday")
+
+print("a\nb\nc")
+
+print("Hello" + "World")
+
+print("Hello\n" + "\nWorld!")
+
+print("1" + "2" + "3")
+
+print("123\n45\n6")
+
+(1+2*3)
+
+(1+2)*3
+
+7/3
+
+7//3
+
+7%3
+
+22/4
+
+22//4
+
+22%4
+
+3**2
+
+2**3
+
+(4+3)//2
+
+6%2
+
+(1+2)*3**4
+
+(3**4)*2+1
+
+print("2+2=", 2+2)
+
+print("50 pounds is ", 50*0.453592, "Kilograms")
